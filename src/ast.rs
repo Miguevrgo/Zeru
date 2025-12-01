22c65d15a8152828ce0ffe961485424a8eff66d4
Miguevrgo <miguevrgo@gmail.com>
2025-12-01 22:49:18 +0100

    Upload Abstract Syntax Tree base code (Expression + Statements)

---
+++ src/ast.rs
@@ -0,0 +1,70 @@
+use crate::token::Token;
+
+pub struct Program {
+    pub statements: Vec<Statement>,
+}
+
+pub enum Statement {
+    Var {
+        name: String,
+        is_const: bool,
+        value: Expression,
+        type_annotation: Option<String>,
+    },
+    Return(Option<Expression>),
+    Break,
+    Continue,
+    While {
+        cond: Expression,
+        body: Box<Statement>,
+    },
+    ForIn {
+        variable: String,
+        iterable: Expression,
+        body: Box<Statement>,
+    },
+    Expression(Expression),
+    Block(Vec<Statement>),
+    Function {
+        name: String,
+        params: Vec<(String, String)>,
+        return_type: Option<String>,
+        body: Vec<Statement>,
+    },
+    If {
+        condition: Expression,
+        then_branch: Box<Statement>,
+        else_branch: Option<Box<Statement>>,
+    },
+}
+
+pub enum Expression {
+    Int(i64),
+    Float(f64),
+    StringLit(String),
+    Boolean(bool),
+    None,
+
+    Identifier(String),
+
+    Prefix {
+        operator: Token,
+        right: Box<Expression>,
+    },
+
+    Infix {
+        left: Box<Expression>,
+        operator: Token,
+        right: Box<Expression>,
+    },
+
+    Call {
+        function: Box<Expression>,
+        arguments: Vec<Expression>,
+    },
+
+    Get {
+        object: Box<Expression>,
+        name: String,
+    },
+}
